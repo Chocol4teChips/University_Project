fb8f8de7c561a654d4b5b7cb2cb2dd7e9f243f8d
thanugikung <dragoonity987@gmail.com>
2021-01-05 18:02:58 +0700

    update

--- LP06/main.py
+++ LP06/main.py
@@ -1,9 +1,67 @@
-time = float(input("time : "))
-first_name = str(input("first_name : "))
-last_name = str(input("last_name : "))
-age = int(input("age : "))
-temperature = int(input("temperature : "))
-province = str(input("province : "))
+# program description
+# xxxxxxx
 
-list = ["กรุงเทพมหานคร","จังหวัดกาญจนบุรี","จังหวัดจันทบุรี","จังหวัดฉะเฉิงเทรา","จังหวัดชุมพร",
-"จังหวัดชุมพร","จังหวัดชลบุรี","จังหวัดตราด","จังหวัดตาก","จังหวัดนครนายก"]
+# print author information and date-time when executed
+from datetime import datetime # datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S") # dd/mm/YY H:M:S
+print("Executed time: ", dt_string)
+name = 'xxxxxxxxxxxxxxxxxxxxx'
+std_id = 'xxxxxxxx'
+lab = 'LPxxx'
+print(lab +' Name: '+name+' Student ID: '+ std_id)
+print('-----------------------------------------')
+
+# variables declaration
+
+
+# command scripts
+
+
+provinces = {
+    "1"  : "กรุงเทพมหานคร",
+    "2"  : "จังหวัดกาญจนบุรี",
+    "3"  : "จังหวัดจันทบุรี",
+    "4"  : "จังหวัดฉะเฉิงเทรา",
+    "5"  : "จังหวัดชุมพร",
+    "6"  : "จังหวัดชุมพร",
+    "7"  : "จังหวัดชลบุรี",
+    "8"  : "จังหวัดตราด",
+    "9"  : "จังหวัดตาก",
+    "10" : "จังหวัดนครนายก",
+    "11" : "จังหวัดนครปฐม",
+    "12" : "จังหวัดนนทบุรี",
+    "13" : "จังหวัดปทุมธานี",
+    "14" : "จังหวัดปราจีนบุรี",
+    "15" : "จังหวัดพระนครศรีอยุธยา",
+    "16" : "จังหวัดเพชรบุรี",
+    "17" : "จังหวัดราชบุรี",
+    "18" : "จังหวัดระนอง",
+    "19" : "จังหวัดระยอง",
+    "20" : "จังหวัดลพบุรี",
+    "21" : "จังหวัดสิงห์บุรี",
+    "22" : "จังหวัดสมุทรปราการ",
+    "23" : "จังหวัดสมุทรสงคราม",
+    "24" : "จังหวัดสมุทรสาคร",
+    "25" : "จังหวัดสุพรรณบุรี",
+    "26" : "จังหวัดสระแก้ว",
+    "27" : "จังหวัดสระบุรี",
+    "28" : "จังหวัดอ่างทอง",
+    "0"  : "อื่นๆ"
+}
+
+persondict = {
+    "date_time" : dt_string,
+    "fname" : str(input("Enter first name : ")),
+    "lname" : str(input("Enter last name : ")),
+    "age" : int(input("Enter Age : ")),
+    "body_temp" : float(input("Enter body temperature : ")),
+    "location" : "",
+    "risk" : False,
+    "covid0" : False
+}
+print("Location")
+for x,y in provinces.items():
+    print("{}.{}".format(x,y))
+province = int(input("Enter location number : "))
+if province
